08f970ef62a1c57eb810063b52c76aae8ecd75b0
Erwin <winn92091229@gmail.com>
2019-05-17 17:46:05 -0400

    change url request from axios for get contracts

--- src/modules/contracts/actions.js
+++ src/modules/contracts/actions.js
@@ -3,7 +3,7 @@ import Vue from "vue";
 export async function fetchContracts({ commit }) {
   commit("setLoading", true, { root: true });
   const { data } = await Vue.axios({
-    url: "/contracts.json"
+    url: "contracts.json"
   });
   commit("contracts/setContracts", data, { root: true });
   commit("setLoading", false, { root: true });
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/vrd/dist/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "" : "/"
 };
